496c0245b1a3b5e58ecdb2011b17e2fc4ec94aad
Rocksus <rayantonius@gmail.com>
2025-02-09 21:48:22 +0700

    feat: move health router to handler package

--- internal/delivery/http/router/router.go
+++ internal/delivery/http/router/router.go
@@ -1,14 +1,14 @@
 package router
 
 import (
-	"github.com/Rocksus/fundtract/internal/delivery/http/routes"
+	"github.com/Rocksus/fundtract/internal/delivery/http/handler"
 	"github.com/Rocksus/fundtract/internal/platform/log"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func LoadRoutes(e *echo.Echo) {
-	e.GET("/health", routes.HealthCheck)
+	e.GET("/health", handler.HealthCheck)
 
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
 		// LogValuesFunc is called after the request is completed.
